1e6e205629d8b4d2d84bf54aa0193478956ee136
Arv <pandit.arv@gmail.com>
2023-01-26 09:55:02 -0700

    fixed the layout of contact page- main section streatches and pushes the footer to the bottom in every layout

--- milestoneproject/js/scripts.js
+++ milestoneproject/js/scripts.js
@@ -1,21 +1,20 @@
-
-const mobileMenu = document.querySelector(".mobile-menu");
-const headerNav = document.querySelector("header nav ul");
-const hamFries = document.querySelector('.hamburger-fries')
+const mobileMenu = document.querySelector('.mobile-menu');
+const headerNav = document.querySelector('header nav ul');
+const hamFries = document.querySelector('.hamburger-fries');
 // const headerNavLinks = document.querySelectorAll("header nav ul a");
 
 /**** Header Hamburger Menu ****/
 function toggleHamMenu() {
-  mobileMenu.classList.toggle("active");
-  headerNav.classList.toggle("active");
+	mobileMenu.classList.toggle('active');
+	headerNav.classList.toggle('active');
 }
 
 window.addEventListener('click', (e) => {
-  // console.log(e.target)
-  if(e.target === mobileMenu || e.target === hamFries) {
-    toggleHamMenu()
-  }else if(e.target !== headerNav) {
-            mobileMenu.classList.remove("active");
-        headerNav.classList.remove("active");
-  }
-})
+	// console.log(e.target)
+	if (e.target === mobileMenu || e.target === hamFries) {
+		toggleHamMenu();
+	} else if (e.target !== headerNav) {
+		mobileMenu.classList.remove('active');
+		headerNav.classList.remove('active');
+	}
+});
